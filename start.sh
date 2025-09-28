43c1ff0a3f063e1572c31058ea2e0c2236c92234
Alexander Wagner <github@alexanderwagnerdev.com>
2025-09-28 20:12:40 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,5 +5,8 @@ Xvfb :0 -screen 0 1280x720x24 &
 sleep 2
 lxsession &
 obs &
-x11vnc -display :0 -rfbport 5900 -rfbauth /root/.vnc/passwd -forever -shared &
-/usr/share/novnc/utils/novnc_proxy --vnc localhost:5900 --listen 6080
+mkdir -p $HOME/.vnc
+x11vnc -storepasswd $VNC_PASS $HOME/.vnc/passwd
+x11vnc -display :0 -rfbauth $HOME/.vnc/passwd -forever -shared &
+/usr/share/novnc/utils/novnc_proxy --vnc localhost:5900 --listen 6080 &
+wait
