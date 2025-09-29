9f0e651b045073864153ce893e887dedd0b4bae3
Alexander Wagner <github@alexanderwagnerdev.com>
2025-09-29 14:20:12 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
+set -e
 
 export DISPLAY=:0
-Xvfb :0 -screen 0 1280x720x24 &
-sleep 2
-lxsession &
-obs &
+
+if [[ -z "$VNC_PASS" ]]; then
+  echo "VNC_PASS not set!"
+  exit 1
+fi
+
 mkdir -p $HOME/.vnc
 x11vnc -storepasswd $VNC_PASS $HOME/.vnc/passwd
-x11vnc -display :0 -rfbauth $HOME/.vnc/passwd -forever -shared &
-/usr/share/novnc/utils/novnc_proxy --vnc localhost:5900 --listen 6080 &
+
+Xvfb :0 -screen 0 1920x1080x24 > /tmp/xvfb.log 2>&1 &
+sleep 2
+lxsession > /tmp/lxsession.log 2>&1 &
+obs > /tmp/obs.log 2>&1 &
+
+x11vnc -display :0 -rfbauth $HOME/.vnc/passwd -forever -shared > /tmp/x11vnc.log 2>&1 &
+/usr/share/novnc/utils/novnc_proxy --vnc localhost:5900 --listen 6080 > /tmp/novnc.log 2>&1 &
+
 wait
